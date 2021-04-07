d2697a81414ce686c1943744aa59ebe8235700f4
l3nnartt <loesche.lennart@gmail.com>
2021-04-07 22:21:42 +0200

    Timolia-Addon-v1

--- build/tmp/expandedArchives/retromappedReplacedMain.jar_63n45xa63t7eritp3hp103x25/de/labymod/lennart/addon.java
+++ build/tmp/expandedArchives/retromappedReplacedMain.jar_63n45xa63t7eritp3hp103x25/de/labymod/lennart/addon.java
@@ -1,19 +1,27 @@
 package de.labymod.lennart;
 
+import com.google.gson.JsonObject;
 import de.labymod.lennart.listener.MessageReceiveListener;
 import net.labymod.api.LabyModAddon;
-import net.labymod.main.LabyMod;
+import net.labymod.settings.elements.BooleanElement;
+import net.labymod.settings.elements.ControlElement;
 import net.labymod.settings.elements.SettingsElement;
-import net.labymod.utils.Consumer;
-import net.labymod.utils.ServerData;
+import net.labymod.settings.elements.StringElement;
+import net.labymod.utils.Material;
 
 import java.util.List;
 
 public class addon extends LabyModAddon {
 
+    public boolean enabledAutoGG1vs1;
+    public String win1vs1;
+    public String lose1vs1;
+    public static addon INSTANCE;
+
     @Override
     public void onEnable() {
-        System.out.println("Addon enabled yeah");
+        INSTANCE = this;
+        System.out.println("Timolia-Addon enabled");
 
         this.getApi().getEventManager().register(new MessageReceiveListener());
     }
@@ -21,12 +29,18 @@ public class addon extends LabyModAddon {
     @Override
     public void loadConfig() {
 
+        this.enabledAutoGG1vs1 = getConfig().has( "enabledAutoGG1vs1" ) ? getConfig().get( "enabledAutoGG1vs1" ).getAsBoolean() : true;
+        this.win1vs1 = getConfig().has( "win1vs1" ) ? getConfig().get( "win1vs1" ).getAsString() : "gg";
+        this.lose1vs1 = getConfig().has( "lose1vs1" ) ? getConfig().get( "lose1vs1" ).getAsString() : "gg";
+
     }
 
     @Override
-    protected void fillSettings(final List<SettingsElement> list) {
+    protected void fillSettings( List<SettingsElement> subSettings ) {
 
-
+        subSettings.add(new BooleanElement("AutoGG-1vs1", this, new ControlElement.IconData(Material.LEVER), "enabledAutoGG1vs1", this.enabledAutoGG1vs1));
+        subSettings.add(new StringElement("WinGG", this, new ControlElement.IconData(Material.EMPTY_MAP ), "win1vs1", this.win1vs1));
+        subSettings.add(new StringElement("LoseGG", this, new ControlElement.IconData(Material.EMPTY_MAP ), "lose1vs1", this.lose1vs1));
 
     }
 
--- src/main/java/de/labymod/lennart/listener/MessageReceiveListener.java
+++ src/main/java/de/labymod/lennart/listener/MessageReceiveListener.java
@@ -1,5 +1,6 @@
 package de.labymod.lennart.listener;
 
+import de.labymod.lennart.addon;
 import net.labymod.api.events.MessageReceiveEvent;
 import net.minecraft.client.Minecraft;
 
@@ -7,13 +8,14 @@ public class MessageReceiveListener implements MessageReceiveEvent {
 
     @Override
     public boolean onReceive(String s, String s1) {
-        if (s1.contains("»") && s1.contains("1vs1")) {
+        if (!addon.INSTANCE.enabledAutoGG1vs1) return false;
+        if (s1.contains("»") && s1.contains("1vs1") || s1.contains("Splun")) {
             if (s1.contains("gewonnen")) {
-                Minecraft.getMinecraft().thePlayer.sendChatMessage("gg");
+                Minecraft.getMinecraft().thePlayer.sendChatMessage(addon.INSTANCE.win1vs1);
             } else if (s1.contains("verloren")) {
-                Minecraft.getMinecraft().thePlayer.sendChatMessage("gg");
+                Minecraft.getMinecraft().thePlayer.sendChatMessage(addon.INSTANCE.lose1vs1);
             }
-        }
-        return false;
+        } return false;
     }
+
 }
