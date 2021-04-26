2c6e02536dc98992c122ad556135c72693556624
l3nnartt <loesche.lennart@gmail.com>
2021-04-27 00:15:50 +0200

    Initial Commit

---
+++ src/main/java/de/labymod/lennart/autogglistener/MessageReceiveBrainbowListener.java
@@ -0,0 +1,20 @@
+package de.labymod.lennart.autogglistener;
+
+import de.labymod.lennart.TimoliaAddon;
+import net.labymod.api.events.MessageReceiveEvent;
+import net.minecraft.client.Minecraft;
+
+public class MessageReceiveBrainbowListener implements MessageReceiveEvent {
+    @Override
+    public boolean onReceive(String s, String s1) {
+
+        if (!TimoliaAddon.getINSTANCE().isEnabledAutoGGBrainbow()) return false;
+
+        if (TimoliaAddon.getINSTANCE().isBrainbow()) {
+            if (s1.contains("Noch") && s1.contains("bis zum nächsten Rang!")) {
+                Minecraft.getMinecraft().thePlayer.sendChatMessage(TimoliaAddon.getINSTANCE().getGameBrainbow());
+            }
+        } return false;
+
+    }
+}
---
+++ src/main/java/de/labymod/lennart/listener/TablistHeaderMapListener.java
@@ -0,0 +1,36 @@
+package de.labymod.lennart.listener;
+
+import de.labymod.lennart.TimoliaAddon;
+import net.labymod.api.events.TabListEvent;
+import net.minecraft.client.Minecraft;
+
+import java.util.List;
+
+
+public class TablistHeaderMapListener implements TabListEvent {
+
+    @Override
+    public void onUpdate(Type type, String header, String s1) {
+        if (containsTimoliaServer(header)) {
+            Minecraft.getMinecraft().thePlayer.sendChatMessage("/v");
+            TimoliaAddon.getINSTANCE().setListenForMap(true);
+        }
+        MessageMapReceiveListener.latestMap = null;
+    }
+
+    private boolean containsTimoliaServer(String gamemode, String s) {
+        return s.contains("§r§7 Du spielst auf §r§6§o" + gamemode);
+    }
+
+    private boolean containsTimoliaServer(String s) {
+        List<String> serverList = TimoliaAddon.getINSTANCE().getAddonConfig().getServers();
+        for (String server : serverList) {
+
+            if (containsTimoliaServer(server, s)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+}
