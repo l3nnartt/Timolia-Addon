968422a8666aaa3d67b0201e8c699e3909ba2435
l3nnartt <loesche.lennart@gmail.com>
2021-05-22 15:39:49 +0200

    fix hoffe ich

---
+++ src/main/java/de/labymod/lennart/ConfigItem.java
@@ -0,0 +1,35 @@
+package de.labymod.lennart;
+
+import static net.labymod.settings.elements.ControlElement.*;
+
+public class ConfigItem {
+
+    private String gamemode;
+    private String configMessage;
+    private boolean configEnabledValue;
+    private IconData icon;
+
+    public ConfigItem(String gamemode, String configMessage, boolean configEnabledValue, IconData icon) {
+        this.gamemode = gamemode;
+        this.configMessage = configMessage;
+        this.configEnabledValue = configEnabledValue;
+        this.icon = icon;
+    }
+
+
+    public String getGamemode() {
+        return gamemode;
+    }
+
+    public String getConfigMessage() {
+        return configMessage;
+    }
+
+    public boolean isConfigEnabledValue() {
+        return configEnabledValue;
+    }
+
+    public IconData getIcon() {
+        return icon;
+    }
+}
