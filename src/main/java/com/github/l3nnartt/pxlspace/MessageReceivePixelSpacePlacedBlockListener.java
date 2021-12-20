09adfaaab32a91c605b26b966319342be8687740
l3nnartt <loesche.lennart@gmail.com>
2021-12-20 17:39:50 +0100

    Revert "pxlspace stats gibts jetzt serverseitig, karmapunkte werden nun auf der website angezeigt"
    This reverts commit 80f56a5d8721858e67f1badc87ea602494fa504e.

---
+++ src/main/java/com/github/l3nnartt/pxlspace/MessageReceivePixelSpacePlacedBlockListener.java
@@ -0,0 +1,68 @@
+package com.github.l3nnartt.pxlspace;
+
+import com.github.l3nnartt.TimoliaAddon;
+import net.labymod.api.events.MessageReceiveEvent;
+import net.labymod.core.LabyModCore;
+import net.labymod.main.LabyMod;
+import net.minecraft.block.properties.IProperty;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.client.Minecraft;
+import net.minecraft.util.BlockPos;
+import net.minecraft.util.EnumChatFormatting;
+import net.minecraft.util.MovingObjectPosition;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Map;
+
+public class MessageReceivePixelSpacePlacedBlockListener implements MessageReceiveEvent {
+
+    private String farbe = null;
+
+    @Override
+    public boolean onReceive(String s, String msg) {
+        if (TimoliaAddon.getInstance().isEnabledPxlSpaceStats()) {
+            if (msg.contains("Timolia» Du hast einen Block platziert! In 20 Sekunden kannst du den nächsten bauen!")) {
+                TimoliaAddon.getInstance().addplacedBlocks();
+                getBlockColor();
+                if (TimoliaAddon.getInstance().getAuthenticator().authenticate()) {
+                    TimoliaAddon.getInstance().getExService().execute(() -> {
+                        try {
+                            HttpURLConnection con = (HttpURLConnection) (new URL("http://karmatop.de/addon/pxl.php?name=" + LabyMod.getInstance().getLabyModAPI().getPlayerUsername() + "&color=" + farbe + "&uuid=" + LabyMod.getInstance().getLabyModAPI().getPlayerUUID())).openConnection();
+                            con.setRequestProperty("User-Agent",
+                                    "Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10.4; en-US; rv:1.9.2.2) Gecko/20100316 Firefox/3.6.2");
+                            con.connect();
+                            int code = con.getResponseCode();
+                            if (code == 200) {
+                                System.out.println("Timolia Addon » Block added");
+                            } else {
+                                System.out.println(code);
+                            }
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    });
+                }
+            }
+        } else {
+            return false;
+        } return false;
+    }
+
+    private void getBlockColor() {
+        MovingObjectPosition movingObjectPosition = LabyModCore.getMinecraft().getPlayer().rayTrace(10, 1.0F);
+        BlockPos blockPos = movingObjectPosition.getBlockPos();
+        IBlockState iblockstate = Minecraft.getMinecraft().theWorld.getBlockState(blockPos);
+
+        for (Map.Entry<IProperty, Comparable> entry : iblockstate.getProperties().entrySet()) {
+            String s = entry.getValue().toString();
+
+            if (entry.getValue() == Boolean.TRUE) {
+                s = EnumChatFormatting.GREEN + s;
+            } else if (entry.getValue() == Boolean.FALSE) {
+                s = EnumChatFormatting.RED + s;
+            }
+            farbe = s;
+        }
+    }
+
+}
---
+++ src/main/java/com/github/l3nnartt/pxlspace/MessageSendStats.java
@@ -0,0 +1,126 @@
+package com.github.l3nnartt.pxlspace;
+
+import com.github.l3nnartt.TimoliaAddon;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import net.labymod.api.events.MessageSendEvent;
+import net.labymod.main.LabyMod;
+import org.apache.commons.io.IOUtils;
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.util.Map;
+
+public class MessageSendStats implements MessageSendEvent {
+
+    String player = null;
+
+    @Override
+    public boolean onSend(final String message) {
+        if (TimoliaAddon.getInstance().isEnabledPxlSpaceStats()) {
+            if (!TimoliaAddon.getInstance().isPixelspace()) return false;
+            String[] args = message.split(" ");
+            if (message.toLowerCase().startsWith("/stats")) {
+                if (args.length > 1) {
+                    player = args[1];
+                } else {
+                    player = LabyMod.getInstance().getPlayerName();
+                }
+                if (TimoliaAddon.getInstance().getAuthenticator().authenticate()) {
+                    TimoliaAddon.getInstance().getExService().execute(() -> {
+                        try {
+                            JsonObject content = getURLContent("http://karmatop.de/addon/pxl-api.php?name=" + player).getAsJsonObject();
+                            if(content.has("status") && content.get("status").getAsInt() == 404){
+                                LabyMod.getInstance().displayMessageInChat(content.get("message").getAsString());
+                            }else {
+                                createStats(content);
+                            }
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    });
+                }
+                return true;
+            }
+        } return false;
+    }
+
+    public JsonElement getURLContent(String url) throws IOException {
+        HttpURLConnection con = (HttpURLConnection) (new URL(url)).openConnection();
+        con.setConnectTimeout(5000);
+        con.connect();
+        String jsonString = IOUtils.toString(con.getInputStream(), StandardCharsets.UTF_8);
+        JsonParser parser = new JsonParser();
+        return  parser.parse(jsonString);
+    }
+
+    public void createStats(JsonObject content) {
+        LabyMod.getInstance().displayMessageInChat("§6░▒▓§bStats von " + player + " §6▓▒░");
+        LabyMod.getInstance().displayMessageInChat("§6╔═════");
+        for (Map.Entry<String, JsonElement> entry : content.entrySet()) {
+            if (!entry.getKey().equals("total") && !entry.getKey().equals("name") && !entry.getKey().equals("uuid")) {
+                LabyMod.getInstance().displayMessageInChat("§6╠ §" + convertColor(entry.getKey()) + "✦ §f" + capitalize(entry.getKey()) + ": §7" + entry.getValue().getAsString());
+            }
+        }
+        LabyMod.getInstance().displayMessageInChat("§6╚═════");
+    }
+
+    public String convertColor(String name){
+        String colorCode = "f";
+        switch (name){
+            case "brown":
+            case "orange":
+                colorCode = "6";
+                break;
+            case "magenta":
+            case "purple":
+                colorCode = "5";
+                break;
+            case "lightBlue":
+                colorCode = "b";
+                break;
+            case "yellow":
+                colorCode = "e";
+                break;
+            case "lime":
+                colorCode = "a";
+                break;
+            case "pink":
+                colorCode = "d";
+                break;
+            case "gray":
+                colorCode = "8";
+                break;
+            case "silver":
+                colorCode = "7";
+                break;
+            case "cyan":
+                colorCode = "3";
+                break;
+            case "blue":
+                colorCode = "9";
+                break;
+            case "green":
+                colorCode = "2";
+                break;
+            case "red":
+                colorCode = "c";
+                break;
+            case "black":
+                colorCode = "0";
+                break;
+            default:
+                colorCode = "f";
+        }
+        return colorCode;
+    }
+    public static String capitalize(String str){
+        if(str == null) {
+            return null;
+        }
+        return str.substring(0, 1).toUpperCase() + str.substring(1);
+    }
+
+}
